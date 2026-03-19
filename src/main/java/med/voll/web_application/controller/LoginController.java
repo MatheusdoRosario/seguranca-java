ac16b8d74d545ecd8649319b93948cbae5e4e563
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-19 16:22:28 -0300

    Implementa tela de confirmacao para logout

--- src/main/java/med/voll/web_application/controller/LoginController.java
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
     @GetMapping("/login")
-    public String carregaPaginaListagem() {
+    public String carregaPaginaLogin() {
         return "autenticacao/login";
+    }
+
+    @GetMapping("/logout")
+    public String carregaPaginaLogout() {
+        return "autenticacao/logout";
     }
 }
