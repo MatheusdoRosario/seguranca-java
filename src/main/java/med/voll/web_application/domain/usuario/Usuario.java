b9a7c451bb5928a62ecd31ae42fe41aeebefaea9
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-20 11:02:19 -0300

    Finaliza a implementacao do Usuario

--- src/main/java/med/voll/web_application/domain/usuario/Usuario.java
+++ src/main/java/med/voll/web_application/domain/usuario/Usuario.java
@@ -1,10 +1,15 @@
 package med.voll.web_application.domain.usuario;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +17,19 @@ public class Usuario {
     private String nome;
     private String email;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
