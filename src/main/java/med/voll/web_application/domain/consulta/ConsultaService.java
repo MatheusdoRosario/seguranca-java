02a78292b0ab4bd3662188baba8af35519b27031
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-25 10:50:50 -0300

    Restringe vizualizacoes

--- src/main/java/med/voll/web_application/domain/consulta/ConsultaService.java
+++ src/main/java/med/voll/web_application/domain/consulta/ConsultaService.java
@@ -5,10 +5,12 @@ import med.voll.web_application.domain.RegraDeNegocioException;
 import med.voll.web_application.domain.medico.MedicoRepository;
 import med.voll.web_application.domain.paciente.Paciente;
 import med.voll.web_application.domain.paciente.PacienteRepository;
+import med.voll.web_application.domain.usuario.Perfil;
 import med.voll.web_application.domain.usuario.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
@@ -25,8 +27,11 @@ public class ConsultaService {
         this.pacienteRepository = pacienteRepository;
     }
 
-    public Page<DadosListagemConsulta> listar(Pageable paginacao) {
-        return repository.findAllByOrderByData(paginacao).map(DadosListagemConsulta::new);
+    public Page<DadosListagemConsulta> listar(Pageable paginacao, Usuario logado) {
+        if (logado.getPerfil() == Perfil.ATENDENTE) {
+            return repository.findAllByOrderByData(paginacao).map(DadosListagemConsulta::new);
+        }
+        return repository.buscaPersonalizadaConsultas(logado.getId(), paginacao).map(DadosListagemConsulta::new);
     }
 
     @Transactional
